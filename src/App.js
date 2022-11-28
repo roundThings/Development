ad8fb2364ec7e16e892c9384e10ea38590d51061
SquareThings <Amy_Xiao@Brown.edu>
2022-11-28 16:37:57 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { useState } from "react";
+import gardenData from "./Assets/rose-data.json";
+import GardenItem from "./components/GardenItem";
+import filterSystem from "./components/filterSystem.js";
+import CheckboxLabels from './components/filterSystem.js';
+import BasicExample from './components/sortSystem';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+gardenData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+
 
 function App() {
+  const [cart, setCart] = useState({});
+  const [total, setTotal] = useState(0);
+  const [type, setType] = useState("All");
+
+  const matchesFilterType = item => {
+    if (type === "All") {
+      return true
+    } else if (type === item.type) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Garden</h1>
+      <div class="MainGrid">
+        <div class="Container1">
+          {gardenData.map((item, index) => (
+            <GardenItem{...item} cart={cart} setCart={setCart} total={total} setTotal={setTotal} setType={setTotal} />
+          ))}
+        </div>
+        <div class="SideBar">
+          <div>
+            <div class="Container2">
+              <h2>Sort</h2>
+              {BasicExample()}
+            </div>
+            <h2>Filter</h2>
+
+            <h2>Cart</h2>
+            {Object.keys(cart).map((key, index) => <div>{cart[key]}x {key}</div>)}
+            <h1>Total: ${(total).toFixed(2)}</h1>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
-}
+};
 
 export default App;
---
+++ src/components/GardenItem.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import "../App.css";
+
+export default function GardenItem(props) {
+
+    function handleClickAdd() {
+        props.setCart(addToCart(props.cart, props.name))
+        props.setTotal(addToTotal(props.total, props.price))
+    };
+
+    function handleClickRemove() {
+        props.setTotal(removeFromTotal(props.cart, props.total, props.price, props.name))
+        props.setCart(removeFromCart(props.cart, props.name))
+
+    };
+
+    function addToCart(cart, name) {
+
+        if (cart.hasOwnProperty(name)) {
+            cart[name] += 1
+        }
+        else {
+            cart[name] = 1
+        }
+        return cart
+    };
+
+    function removeFromCart(cart, name) {
+        if (cart[name] > 1) {
+            cart[name] -= 1
+        }
+        else if (cart[name] == 1) {
+            delete cart[name]
+        }
+        return cart
+    };
+
+    function addToTotal(total, price) {
+        return total += price
+    };
+
+    function removeFromTotal(cart, total, price, name) {
+        if (cart.hasOwnProperty(name)) {
+            total -= price
+        }
+        else {
+            total = total
+        }
+        return total
+
+    };
+
+    return <div class="Item">
+        <h1>{props.name}</h1>
+        <img src={props.image}></img>
+        <p>{props.description}</p>
+        <p>{props.type}</p>
+        <h2>${props.price}</h2>
+        <button onClick={handleClickAdd}>Add
+        </button>
+        <button onClick={handleClickRemove}>Remove
+        </button>
+    </div>
+
+
+}
+
+
+
