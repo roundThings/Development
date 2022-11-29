02d53e5248400c87307868a8e189afe40c20080a
SquareThings <Amy_Xiao@Brown.edu>
2022-11-28 20:46:38 -0500

    with Changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function App() {
   const [cart, setCart] = useState({});
   const [total, setTotal] = useState(0);
   const [type, setType] = useState("All");
+  const [color, setColor] = useState("All");
+  const [sort, setSort] = useState("High to Low")
   const matchesFilterType = item => {
     // all items should be shown when no filter is selected
     if (type === "All") {
@@ -29,12 +31,50 @@ function App() {
       return false
     }
   }
+
+  const matchesFilterColor = item => {
+    // all items should be shown when no filter is selected
+    if (color === "All") {
+      return true
+    } else if (color === item.color) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const reset = general => {
+    setColor("All")
+    setType("All")
+    setSort("High to Low")
+    setCart({})
+    setTotal(0)
+  };
+
+  const selectFilterColor = eventKey => {
+    setColor(eventKey);
+  };
+
   const selectFilterType = eventKey => {
     setType(eventKey);
   };
 
-  const filteredData = gardenData.filter(matchesFilterType)
+  const selectSortType = eventKey => {
+    setSort(eventKey);
+  };
 
+  const sortData = gardenData => {
+    if (sort === "High to Low") {
+      return gardenData.sort((a, b) => b.price - a.price);
+    }
+    else if (sort === "Low to High") {
+      return gardenData.sort((a, b) => a.price - b.price);
+    };
+  }
+
+  const firstFilteredData = gardenData.filter(matchesFilterType)
+  const filteredData = firstFilteredData.filter(matchesFilterColor)
+  const sortedData = sortData(gardenData)
 
   return (
     <div className="App">
@@ -49,14 +89,17 @@ function App() {
           <div>
             <div class="Container2">
               <h2>Sort</h2>
-              <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </DropdownButton>
+              <Nav id="sort" variant="pills" defaultActiveKey="High to Low" onSelect={selectSortType}>
+                <Nav.Item>
+                  <Nav.Link eventKey="High to Low"> High to Low </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="Low to High">Low to High</Nav.Link>
+                </Nav.Item>
+              </Nav>
             </div>
-            <h2>Filter</h2>
-            <Nav variant="pills" defaultActiveKey="All" onSelect={selectFilterType}>
+            <h2>Type</h2>
+            <Nav id="type" variant="pills" defaultActiveKey="All" onSelect={selectFilterType}>
               <Nav.Item>
                 <Nav.Link eventKey="All"> All</Nav.Link>
               </Nav.Item>
@@ -70,9 +113,31 @@ function App() {
                 <Nav.Link eventKey="Climber">Climber</Nav.Link>
               </Nav.Item>
             </Nav>
+            <h2>Color</h2>
+            <Nav id="color" variant="pills" defaultActiveKey="All" onSelect={selectFilterColor}>
+              <Nav.Item>
+                <Nav.Link eventKey="All"> All</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Pink">Pink</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Yellow">Yellow</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Red">Red</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Multi">Multi</Nav.Link>
+              </Nav.Item>
+            </Nav>
             <h2>Cart</h2>
             {Object.keys(cart).map((key, index) => <div>{cart[key]}x {key}</div>)}
             <h1>Total: ${(total).toFixed(2)}</h1>
+            <div class="Container3">
+              <button onClick={reset}>Reset
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -80,5 +145,4 @@ function App() {
 
   );
 };
-
 export default App;
